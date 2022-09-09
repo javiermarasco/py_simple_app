c47a6d889d2d8d8226ea08aab956328d250f26f7
Javier Marasco <javier.marasco@gmail.com>
2022-09-09 10:08:59 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
+hostname = "localhost"
+port = os.getenv('PORT')
+
+class Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.message = os.getenv('MESSAGE')
+        self.send_response(200)
+        self.send_header("content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(" Message ==> " + self.message, encoding='utf-8') )
+
+runningServer = HTTPServer((hostname, port), Server)
+print("Web server started.")
+
+try:
+    runningServer.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+runningServer.server_close()
+print("Web server stopped.")
