57153d097aaa35adc375363673ec891799b6c2cd
Javier Marasco <javier.marasco@amadeus.com>
2022-12-04 18:36:01 +0100

    Split getter from server and fixed getter

--- getter.py
+++ getter.py
@@ -2,10 +2,19 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
 import time
 import requests
+import json
 
-dest_url = "http://localhost:30000" #os.getenv('DEST_URL')
+# dest_url = "http://localhost:30000" #os.getenv('DEST_URL')
+
+loop_wait_time = os.getenv('LOOPWAITTIME')
+inputs = json.loads(os.getenv('INPUT'))
+
+#urls = inputs.urls  
 
 while True:
-    response = requests.get(url=dest_url)
-    print(response.text)
-    time.sleep(10)
+    for input in inputs:
+        response = requests.get(url=input['url'])
+        print(f"getting from {input['url']} and now waiting {input['cadence']} seconds to get the next url.")
+        time.sleep(int(input['cadence']))
+        print(f"Waiting {int(loop_wait_time)} seconds before next loop.")
+    time.sleep(int(loop_wait_time))
